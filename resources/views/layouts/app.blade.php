ac6a073ea9b93e87b9fead01f3bf13061c54468b
Mohammad Arman <arman.ctgbd1394@gmail.com>
2021-03-03 01:18:16 +0600

    Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,15 +58,6 @@
     </div>
     <!-- /site content wrapper -->
 
-    <!-- Theme Chooser -->
-    <div class="dt-customizer-toggle">
-      <a href="javascript:void(0)" data-toggle="customizer"> <i class="icon icon-spin icon-setting"></i> </a>
-    </div>
-    <!-- /theme chooser -->
-
-    <!-- Customizer Sidebar -->
-    <x-right-sidebar/>
-    <!-- /customizer sidebar -->
 
   </main>
 </div>
